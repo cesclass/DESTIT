1f1270e6d73f285acf2d69da22af81a3db82744b
Dylan Caron <dcaron81.dc@gmail.com>
2020-04-21 15:06:36 +0200

    Ajout des photos pour les auteurs

--- frontend/src/Publication.vue
+++ frontend/src/Publication.vue
@@ -1,11 +1,16 @@
 <template>
   <article class="publication">
     <img class="image"
-      src="./assets/placeholder.png" 
+      src="./assets/placeholder_img.png" 
       :alt="titre">
     <div class="presentation">
       <a href="#" class="titre"> <h2> {{ titre }} </h2> </a>
-      <a href="#" class="auteur"> <h3> {{ auteur }} </h3> </a>
+      <a href="#" class="auteur" v-for="auteur in auteurs"> <h3>
+        <img class="miniature"
+          src="./assets/placeholder_profil.jpg"
+          :alt="auteur">
+         {{ auteur }} 
+      </h3> </a>
       <a href="#" class="ouvrage"> <h3> {{ ouvrage }} </h3> </a>
       <h3 class="date"> {{ date }} </h3>
     </div>
@@ -18,9 +23,9 @@ export default {
   data () {
     return {
       titre: "Ma nouvelle publication",
-      auteur: "Dylan Caron",
+      auteurs: ["Dylan Caron", "Cyril Esclassan", "Florian Azizen", "Nicolas Augier"],
       ouvrage: "The conference",
-      date: "20/07/1996",
+      date: "29/47/53",
       resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus eros sed leo consequat molestie. Aenean nec tellus mollis, viverra sapien quis, feugiat mi. Nulla ipsum purus, pharetra eget fermentum ut, imperdiet vitae leo. Phasellus ut gravida mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel erat sollicitudin, sollicitudin sapien at, faucibus odio. Suspendisse potenti. Integer posuere nisi eget viverra euismod. Aliquam neque odio, faucibus sit amet mollis vel, lacinia quis nisi. Proin iaculis sodales est a tristique. Integer a nisi non purus lobortis mattis at sed lectus. Nullam magna neque, aliquam sed fermentum ut, aliquet vel enim. Maecenas lacinia ante in vulputate porttitor. Maecenas et ultrices dui, vel consequat ligula. Nam ac suscipit lacus. Pellentesque efficitur molestie velit, quis mollis nibh varius eu."
     }
   }, 
