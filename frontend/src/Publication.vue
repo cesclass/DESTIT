0a8f724b7f5d12632aca77990c46ee78a9762282
TankAttaque <48088553+TankAttaque@users.noreply.github.com>
2020-04-21 16:25:08 +0200

    Modification du style des ouvrages dans les publiquations

--- frontend/src/Publication.vue
+++ frontend/src/Publication.vue
@@ -14,8 +14,10 @@
           <h3 v-if="auteur !== auteurs[auteurs.length - 1]">, </h3> 
         </a>
       </div>
-      <a href="#" class="ouvrage"> <h3> {{ ouvrage }} </h3> </a>
-      <h3 class="date"> {{ date }} </h3>
+      <div class="ouvrage">
+        <a href="#" class="nom_ouvrage"> <h3> {{ ouvrage }}&thinsp; </h3> </a>
+        <h3 class="date"> {{ date }} </h3>
+      </div>
     </div>
     <div class="apercu"> {{ apercu }} </div>
   </article>
@@ -27,7 +29,7 @@ export default {
     return {
       titre: "Ma nouvelle publication",
       auteurs: ["Dylan Caron", "Cyril Esclassan", "Florian Azizen", "Nicolas Augier"],
-      ouvrage: "The conference",
+      ouvrage: "The best conference ever",
       date: "29/47/53",
       resume: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus eros sed leo consequat molestie. Aenean nec tellus mollis, viverra sapien quis, feugiat mi. Nulla ipsum purus, pharetra eget fermentum ut, imperdiet vitae leo. Phasellus ut gravida mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vel erat sollicitudin, sollicitudin sapien at, faucibus odio. Suspendisse potenti. Integer posuere nisi eget viverra euismod. Aliquam neque odio, faucibus sit amet mollis vel, lacinia quis nisi. Proin iaculis sodales est a tristique. Integer a nisi non purus lobortis mattis at sed lectus. Nullam magna neque, aliquam sed fermentum ut, aliquet vel enim. Maecenas lacinia ante in vulputate porttitor. Maecenas et ultrices dui, vel consequat ligula. Nam ac suscipit lacus. Pellentesque efficitur molestie velit, quis mollis nibh varius eu."
     }
