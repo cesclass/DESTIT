c2fc96f90397382d26a5128b2a6d63dc3d6c7ccf
Dylan Caron <dcaron81.dc@gmail.com>
2020-04-24 15:07:38 +0200

    Update CategoryFilter.vue

--- frontend/src/CategoryFilter.vue
+++ frontend/src/CategoryFilter.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="filter_box">
         <div class="box_title">
-            Title
+            Filters
         </div>
         <div class="box_content">
-            <div class="category_name">
-                Categorie
+            <div class="categorie" v-for="cat in categories" :key="cat.name">
+                <div class="name"> {{ cat.name }} </div>
+                <div class="show_menu"> v </div>
             </div>
         </div>
     </div>
@@ -13,7 +14,63 @@
 
 <script>
 export default {
-    
+    data () { return {
+        categories: [
+            {
+                name: "system", 
+                values: [
+                    "computer", 
+                    "smartphone", 
+                    "mobile phone",
+                    "smartwatch"
+                ]
+            }, {
+                name: "input device",
+                values: [
+                    "mouse",
+                    "trackball",
+                    "eyetracker"
+                ]
+            }, {
+                name: "disability",
+                values: [
+                    "motor",
+                    "visualy impaired"
+                ]
+            }, {
+                name: "method",
+                values: [
+                    "key based",
+                    "gesture based"
+                ]
+            }, {
+                name: "navigation",
+                values: [
+                    "direct",
+                    "switch scanning",
+                    "indirect pointer"
+                ]
+            }, {
+                name: "selection",
+                values: [
+                    "discrete",
+                    "continuous"
+                ]
+            }, {
+                name: "prediction system",
+                values: []
+            }, {
+                name: "transformation",
+                values: []
+            }, {
+                name: "output",
+                values: []
+            }, {
+                name: "widget",
+                values: []
+            }
+        ]
+    }}
 }
 </script>
 
