70ab5e12d2c58c4571d6fe7f7535e71e0f49def7
Dylan Caron <dcaron81.dc@gmail.com>
2020-05-05 12:28:07 +0200

    Transformation du formulaire en projet Vue

---
+++ admin/src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="formulaire">
+    <header>
+      <h1 class="main_title">DESTIT</h1>
+    </header>
+
+    <div class="main_form">
+      <formulaire v-for="category in categories" :key="category.name"
+          :cat="category">
+      </formulaire>
+    </div>
+  </div>
+</template>
+
+<script>
+import formulaire from "./Form.vue"
+
+export default {
+  components: {
+    formulaire
+  }, 
+
+  name: 'app',
+
+  data () {
+    return {
+      categories: [
+        {
+          name: "system",
+          values: ["computer", "smartphone", "mobile phone", "smartwatch"],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "input device",
+          values: ["mouse", "trackball", "eyetracker"],
+          is_open: false,
+          exclusive: true
+        },
+        {
+          name: "disability",
+          values: ["motor", "visualy impaired"],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "method",
+          values: ["key based", "gesture based"],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "navigation",
+          values: ["direct", "switch scanning", "indirect pointer"],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "selection",
+          values: ["discrete", "continuous"],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "prediction system",
+          values: [],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "transformation",
+          values: [],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "output",
+          values: [],
+          is_open: false,
+          exclusive: false
+        },
+        {
+          name: "widget",
+          values: [],
+          is_open: false,
+          exclusive: false
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style src="../css/app.css">
+</style>
---
+++ admin/src/Form.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form">
+    <h5> {{ cat.name }} </h5>
+
+    <!-- Si les valeurs sont exclusives (radio) -->
+    <div class="tags_container check_type" v-if="cat.exclusive">
+      <div class="tag" v-for="tag in cat.values" :key="tag">
+        <input type="radio" class="radio" :name="cat.name" :value="tag">
+        <label :for="tag"> {{ tag }} </label>
+      </div>
+    </div>
+
+    <!-- Sinon (check) -->
+    <div class="tags_container check_type" v-else>
+      <div class="tag" v-for="tag in cat.values" :key="tag">
+        <input type="checkbox" class="checkbox" :name="tag">
+        <label :for="tag"> {{ tag }} </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cat']
+}
+</script>
+
+<style src="../css/form.css">
+</style>
